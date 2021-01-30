354041ea493298aa59d83784f91ab8967049793f
xshokuninx <75437738+xshokuninx@users.noreply.github.com>
2021-01-30 21:33:11 +0900

    Update game.py

--- cogs/utils/game.py
+++ cogs/utils/game.py
@@ -9,9 +9,10 @@ class Game():
         channel (discord.TextChannel): ゲームを進行するチャンネル
         players (Players): 参加者リスト
         days (int): ゲームの経過日
+        time (str):ゲームの時間（night:夜行動 noon:投票)
         executed (Optional[Player]): 処刑されたプレイヤー
         raided (Optional[Player]): 襲撃されたプレイヤー
-        fortuned (Optional[str]): 占い結果
+        fortuned (Optional[str]): 占いされたプレイヤー
     """
 
     def __init__(self):
@@ -19,6 +20,8 @@ class Game():
         self.channel = None
         self.players = Players()
         self.days = 0
+        self.time = night
         self.executed = None
         self.raided = None
         self.fortuned = None
+        
