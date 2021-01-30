f211e57897348ea1c677732953aae6230b6cf818
xshokuninx <75437738+xshokuninx@users.noreply.github.com>
2021-01-31 00:21:20 +0900

    Update game.py

--- cogs/utils/game.py
+++ cogs/utils/game.py
@@ -6,9 +6,11 @@ class Game():
     """人狼ゲーム
     Attributes:
         casting (str):簡易版配役
+        castct (int):配役人数
         status (str): 進行状況
         channel (discord.TextChannel): ゲームを進行するチャンネル
         players (Players): 参加者リスト
+        playct (int): 参加人数
         days (int): ゲームの経過日
         time (str):ゲームの時間（night:夜行動 noon:投票)
         executed (Optional[Player]): 処刑されたプレイヤー
@@ -18,9 +20,11 @@ class Game():
 
     def __init__(self):
         self.casting =''
+        self.castct = 0
         self.status = 'nothing'
         self.channel = None
         self.players = Players()
+        self.playct = 0
         self.days = 0
         self.time = 'night'
         self.executed = None
