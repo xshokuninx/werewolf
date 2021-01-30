2402058d9b7502342a256dc4b3ddd151ab0e0ce4
xshokuninx <75437738+xshokuninx@users.noreply.github.com>
2021-01-30 17:39:46 +0900

    Update status.py

--- cogs/status.py
+++ cogs/status.py
@@ -18,66 +18,7 @@ class GameStatus(commands.Cog):
             return False
 
         return True
-
-    @commands.command()
-    async def create(self, ctx):
-        """ゲームを作成するコマンド"""
-        if self.bot.game.status == 'playing':
-            await ctx.send('ゲーム中です')
-            return
-        if self.bot.game.status == 'waiting':
-            await ctx.send('既に参加者募集中です')
-            return
-        self.bot.game.status = 'waiting'
-        self.bot.game.channel = ctx.channel
-        await ctx.send('参加者の募集を開始しました')
-
-    @commands.command()
-    async def start(self, ctx):
-        """ゲームを開始するコマンド"""
-        if self.bot.game is None:
-            await ctx.send('まだ参加者を募集していません')
-            return
-        if self.bot.game.status == 'playing':
-            await ctx.send('既にゲーム中です')
-            return
-
-        n = len(self.bot.game.players)
-        role = simple[n]
-        role_list = random.sample(role, n)
-        for i in range(n):
-            player = self.bot.game.players[i]
-            user = self.bot.get_user(player.id)
-            role = role_list[i]
-            await user.send(f'あなたの役職は{role}です')
-            if role == '村':
-                continue
-
-            player.set_role(role)
-
-        await ctx.send('役職が配布されました。配布された自分の役職を確認し、準備を完了させてください。')
-        self.bot.game.status = 'playing'
-        await ctx.send('ゲームが開始されました。それぞれの役職にあった行動をとってください。')
-
-    @commands.command()
-    async def set_nothing(self, ctx):
-        self.bot.game.status = 'nothing'
-        await ctx.send(f'game.status を {self.bot.game.status} に変更しました')
-
-    @commands.command()
-    async def set_playing(self, ctx):
-        self.bot.game.status = 'playing'
-        await ctx.send(f'game.status を {self.bot.game.status} に変更しました')
-
-    @commands.command()
-    async def set_waiting(self, ctx):
-        self.bot.game.status = 'waiting'
-        await ctx.send(f'game.status を {self.bot.game.status} に変更しました')
-
-    @commands.command()
-    async def game_status(self, ctx):
-        await ctx.send(f'現在の game.status は {self.bot.game.status} です')
-
-
+    
+    
 def setup(bot):
     bot.add_cog(GameStatus(bot))
