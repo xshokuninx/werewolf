0751541d270c731cd6bc3898eac788299e9d77a4
xshokuninx <75437738+xshokuninx@users.noreply.github.com>
2021-01-31 03:23:38 +0900

    Create vote.py

---
+++ cogs/vote.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+from cogs.utils import errors
+from cogs.utils.game import Game
+from cogs.utils.player import Players
+
+class Vote(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def cog_check(self, ctx):
+        if not isinstance(ctx.channel, discord.DMChannel):
+            await self.bot.on_command_error(ctx, errors.NotDMChannel())
+            return False
+        return True
