6ef16b5bd9a4da20771ae6846a89a0eb637a3d13
xshokuninx <75437738+xshokuninx@users.noreply.github.com>
2021-02-01 01:19:36 +0900

    Update vote.py

--- cogs/vote.py
+++ cogs/vote.py
@@ -14,6 +14,16 @@ class Vote(commands.Cog):
             return False
         return True
     
+    """
+   @commands.command()
+    async def wolflist(self, ctx):
+        if self.bot.game.players.get(ctx.author.id).role != '人狼':
+            await ctx.send('あなたは人狼ではありません')
+            return
+        guild = self.bot.game.channel.guild
+        werewolfs = ' '.join(guild.get_member(w.id).display_name for w in self.bot.game.players.werewolfs)
+        await ctx.send(f'人狼[ {werewolfs} ]')
+  """
    
 
 def setup(bot):
