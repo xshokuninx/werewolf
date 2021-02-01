c535b05a6467afce728d475faf1b523e73a6abc7
xshokuninx <75437738+xshokuninx@users.noreply.github.com>
2021-02-02 01:14:48 +0900

    Update vote.py

--- cogs/vote.py
+++ cogs/vote.py
@@ -21,7 +21,7 @@ class Vote(commands.Cog):
             return
         werewolfs =''
         for w in self.bot.game.players.werewolfs:
-            werewolfs = werewolfs + ' ' +w.role
+            werewolfs = werewolfs + ' ' +w.name
         await ctx.send(f'この村の人狼は {werewolfs} です。')
     
 def setup(bot):
