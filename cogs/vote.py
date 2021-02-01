8dba3d24b10aee946ca585ed92c3c82681562bfa
xshokuninx <75437738+xshokuninx@users.noreply.github.com>
2021-02-02 00:55:34 +0900

    Update vote.py

--- cogs/vote.py
+++ cogs/vote.py
@@ -15,13 +15,13 @@ class Vote(commands.Cog):
         return True
     
     @commands.command()
-    async def werewolfs(self, ctx):
+    async def wolflist(self, ctx):
         if self.bot.game.players.get(ctx.author.id).role != '人狼':
             await ctx.send('あなたは人狼ではありません')
             return
         werewolfs =''
         for w in self.bot.game.players.werewolfs.memid:
-            werewolfs = werewolfs + ' ' +w.mention
+            werewolfs = werewolfs + ' ' +w
         await ctx.send(f'この村の人狼は {werewolfs} です。')
     
 def setup(bot):
