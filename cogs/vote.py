f6d59fc51e02c9f1b1ee28006596483051cb8e1f
xshokuninx <75437738+xshokuninx@users.noreply.github.com>
2021-02-02 17:07:48 +0900

    Update vote.py

--- cogs/vote.py
+++ cogs/vote.py
@@ -16,13 +16,19 @@ class Vote(commands.Cog):
     
     @commands.command()
     async def wolflist(self, ctx):
-        if self.bot.game.players.get(ctx.author.id).role != '人狼':
-            await ctx.send('あなたは人狼ではありません')
+        if self.bot.game.players.get(ctx.author.id).role != '人狼' and self.bot.game.players.get(ctx.author.id).role != '狂信者':
+            await ctx.send('この村の人狼は [********] です。')
+            await ctx.send('この村の狂信者は [********] です。')
             return
         werewolfs =''
         for w in self.bot.game.players.werewolfs:
             werewolfs = werewolfs + ' ' +w.name
-        await ctx.send(f'この村の人狼は {werewolfs} です。')
+        await ctx.send(f'この村の人狼は [{werewolfs}] です。')
+        
+        kyosins =''
+        for k in self.bot.game.players.kyosins:
+            kyosins = kyosins + ' ' +k.name
+        await ctx.send(f'この村の狂信者は [{kyosins}] です。')
     
 def setup(bot):
     return bot.add_cog(Vote(bot))
