5a7eee33906e32ae0a97ac605d976e76af9d273e
xshokuninx <75437738+xshokuninx@users.noreply.github.com>
2021-02-03 21:05:15 +0900

    Update player.py

--- cogs/utils/player.py
+++ cogs/utils/player.py
@@ -30,7 +30,7 @@ class Player():
         return self
     
     def set_co(self, co: str):
-        self.co = co
+        self.name = co
         return self
     
     def set_name(self, name: str):
