d9acecba26fbd9061a35dd842608c8e9744291d4
xshokuninx <75437738+xshokuninx@users.noreply.github.com>
2021-02-15 03:13:20 +0900

    Update players.py

--- cogs/players.py
+++ cogs/players.py
@@ -78,6 +78,13 @@ class PlayersCog(commands.Cog):
         await ctx.send(f"配役人数:{self.bot.game.castct}　参加人数:{self.bot.game.playct}")
         
     @commands.command()
+    async def panya(self, ctx):
+        self.bot.game.casting = self.bot.game.casting + 'え'
+        self.bot.game.castct += 1
+        await ctx.send(f"今の配役:{self.bot.game.casting}")
+        await ctx.send(f"配役人数:{self.bot.game.castct}　参加人数:{self.bot.game.playct}")
+        
+    @commands.command()
     async def jinro(self, ctx):
         if self.bot.game.status == "nothing":
             return await ctx.send("現在ゲームはありません。")
